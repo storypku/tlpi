dc8c0bfd49ba62adeeb2398492150f6902faefe3
storypku <storypku@gmail.com>
2013-07-29 11:24:54 +0800

    updated version of dynamic array with remove and pop implemented with capacity shrink

--- Data.Structures.and.Algorithms.in.Python/dynamic_array.py
+++ Data.Structures.and.Algorithms.in.Python/dynamic_array.py
@@ -35,6 +35,47 @@ class DynamicArray:
         self._array[self._length] = obj
         self._length += 1
 
+    def insert(self, k, value):
+        """Insert value at index k, shifting subsequent values rightward."""
+        # for simplicity, we assume 0<=k<=n in this version
+        if not 0 <= k <= self._length:
+            raise IndexError("invalid index")
+        if self._length == self._capacity:
+            self._resize(2 * self._capacity)
+        for j in range(self._length, k, -1):
+            self._array[j] = self._array[j-1]
+        self._array[k] = value
+        self._length += 1
+
+    def __delitem__(self, k):
+        """Remove value at index k."""
+        self.pop(k)
+
+    def pop(self, k=None):
+        """Pop value at index k, shifting subsequent values leftward."""
+        if k is None:   # default k
+            k = self._length - 1
+
+        if self._length == 0:
+            raise IndexError("pop from empty list")
+
+        # for simplicity, we assume 0<=k<n in this version.
+        if not 0 <= k < self._length:
+            raise IndexError("index out of range")
+
+        value = self._array[k]
+
+        for j in range(k, self._length-1):
+            self._array[j] = self._array[j+1]   # shift others to fill gap
+
+        self._array[self._length -1] = None     # help garbage collection
+        self._length -= 1
+
+        if self._length < self._capacity // 2 : # shrink the dynamic array
+            self._resize(self._capacity // 2)
+
+        return value
+
     def _resize(self, cap):
         """Resize internal array to capacity cap."""
         newArray = self._make_array(cap)
@@ -46,3 +87,22 @@ class DynamicArray:
     def _make_array(self, cap):      # non-public utility
         """Return new array with capacity cap."""
         return (cap * ctypes.py_object)()
+
+    def remove(self, value):
+        """Remove first occurrence of value (or raise ValueError)."""
+
+        for k in range(self._length):
+            if self._array[k] == value:
+
+                for j in range(k, self._length -1):
+                    self._array[j] = self._array[j+1]
+
+                self._array[self._length - 1] = None
+                self._length -= 1
+
+                if self._length < self._capacity // 2: # shrink the array
+                    self._resize(self._capacity // 2)
+
+                return                                 # exit immediately
+
+        raise ValueError("value not found")
