29aba3c594b28fbcd7185a273a756e686658ca8d
Jiaming Liu <storypku@gmail.com>
2013-08-06 00:39:41 +0800

    separate chaining

--- Data.Structures.and.Algorithms.in.Python/map.py
+++ Data.Structures.and.Algorithms.in.Python/map.py
@@ -1,4 +1,5 @@
 from collections import MutableMapping
+from random import randrange
 
 class MapBase(MutableMapping):
     """Abstract Map base class that includes a nonpublic _Item class."""
@@ -38,7 +39,7 @@ class UnsortedTableMap(MapBase):
     def __setitem__(self, k, v): # O(n)
         """Assign value v to key k, overwriting existing value if present."""
         for item in self._table:
-            if k == item.__key:
+            if k == item._key:
                 item._value = v
                 return
         self._table.append(self._Item(k, v))
@@ -68,3 +69,77 @@ def hash_code(s):
         h = (h << 5 & mask) | (h >> 27)
         h += ord(character)
     return h
+
+class HashMapBase(MapBase):
+    """Abstract base class for map using hash-table with MAD (Multiply, Add
+    and Divide) compression."""
+
+    def __init__(self, cap=11, p=109345121):
+        self._table = cap * [None]
+        self._n = 0 # number of entries in the map
+        self._prime = p # prime for MAD compression
+        self._scale = 1 + randrange(p-1) # scale from 1 to p-1 for MAD
+        self._shift = randrange(p) # shift from 0 to p-1 for MAD
+
+    def _hash_function(self, k):
+        return (hash(k) * self._scale + self._shift)\
+                % self._prime % len(self._table)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, k):
+        j = self._hash_function(k)
+        print("====", j)
+        return self._bucket_getitem(j, k) # may raise KeyError
+
+    def __setitem__(self, k, v):
+        j = self._hash_function(k)
+        print("====", j)
+        self._bucket_setitem(j, k, v)   # subroutine maintains self._n
+        if self._n > len(self._table) // 2: # keep load factor <= 0.5
+            self._resize(2 * len(self._table) + 1)
+
+    def __delitem__(self, k):
+        j = self._hash_function(k)
+        print("====", j)
+        self._bucket_delitem(j, k)  # may raise KeyError
+        self._n -= 1
+
+    def _resize(self, c): # Resize bucket array to capacity c
+        old = list(self.items())
+        self._table = c * [None]
+        self._n = 0
+        for (k, v) in old:
+            self[k] = v
+
+class ChainHashMap(HashMapBase):
+    """Hash map implemented with separate chaining for collision
+    resolution."""
+
+    def _bucket_getitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError("Key Error: " + repr(k))
+        return bucket[k]    # may raise KeyError
+
+    def _bucket_setitem(self, j, k, v):
+        if self._table[j] is None:
+            self._table[j] = UnsortedTableMap()
+        oldsize = len(self._table[j])
+        self._table[j][k] = v
+        if len(self._table[j]) > oldsize:
+            self._n += 1
+
+    def _bucket_delitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError("Key Error: " + repr(k))
+        del bucket[k]
+
+    def __iter__(self):
+        for bucket in self._table:
+            if bucket is not None:
+                for key in bucket:
+                    yield key
+
