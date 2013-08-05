977c535438d693de6caf6a9a95245010dfaf19fb
storypku <storypku@gmail.com>
2013-08-05 14:26:11 +0800

    AdaptableHeapPriorityQueue

--- Data.Structures.and.Algorithms.in.Python/priority_queue.py
+++ Data.Structures.and.Algorithms.in.Python/priority_queue.py
@@ -1,5 +1,6 @@
 from custom_exception import Empty
 from linked_deque import PositionalList
+
 class PriorityQueueBase:
     """Abstract base class for a priority queue."""
 
@@ -183,3 +184,60 @@ class HeapPriorityQueue(PriorityQueueBase):
         self._downheap(0)
         return (item._key, item._value)
 
+class AdaptableHeapPriorityQueue(HeapPriorityQueue):
+    """A locator-based priority queue implemented with a binary heap. Effort
+    to make better support for Locator is under the way.
+    """
+
+    # ----  nested Locator class  -----
+    class Locator(HeapPriorityQueue._Item):
+        """Token for locating an entry of the priority queue."""
+        __slots__ = "_index"
+
+        def __init__(self, k, v, j):
+            super().__init__(k, v)
+            self._index = j
+
+    # ----  nonpublic behaviors  -------
+    # override _swap to record new indices
+    def _swap(self, i, j):
+        super()._swap(i, j)
+        self._data[i]._index = i  # reset locator index (pos-swap)
+        self._data[j]._index = j
+
+    def _bubble(self, j):
+        """bubbling of element at index j."""
+        if j > 0 and self._data[j] < self._data[self._parent(j)]:
+            self._upheap(j)
+        else:   # j == 0 or self._data[j] >= self._data[self._parent(j)]
+            self._downheap(j)
+
+    def add(self, key, value):
+        """Add a key-value pair."""
+        token = self.Locator(key, value, len(self._data))
+        self._data.append(token)
+        self._upheap(len(self._data) - 1)
+        return token
+
+    def update(self, loc, newkey, newval):
+        """Update the key and value for the entry identified by Locator
+        loc."""
+        j = loc._index
+        if not (0 <= j < len(self._data) and self._data[j] is loc):
+            raise ValueError("Invalid locator.")
+        loc._key = newkey
+        loc._value = newval
+        self._bubble(j)
+
+    def remove(self, loc):
+        """Remove and return the (k, v) pair identified by Locator loc."""
+        j = loc._index
+        if not (0 <= j < len(self._data) and self._data[j] is loc):
+            raise ValueError("Invalid locator.")
+        if j == len(self) - 1:
+            self._data.pop()
+        else:
+            self._swap(j, len(self) - 1)
+            self._data.pop()
+            self._bubble(j)
+        return (loc._key, loc._value)
