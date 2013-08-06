49fd5b87e4cb0fa063bdbe4e205407906322177d
storypku <storypku@gmail.com>
2013-08-06 12:29:45 +0800

    ProbeHashMap

--- Data.Structures.and.Algorithms.in.Python/map.py
+++ Data.Structures.and.Algorithms.in.Python/map.py
@@ -140,3 +140,55 @@ class ChainHashMap(HashMapBase):
                 for key in bucket:
                     yield key
 
+class ProbeHashMap(HashMapBase):
+    """Hash map implemented with linear probing ofor collision resolution."""
+    _AVAIL = object()   # sentinal marks locations of previous deletions
+
+    def _is_available(self, j):
+        """Return True if index j is available in table."""
+        return self._table[j] is None or self._table[j] is ProbeHashMap._AVAIL
+
+    def _find_slot(self, j, k):
+        """Search for k in bucket at index j.
+
+        Return (success, index) tuple, described as follows:
+        If match was found, success is True and index denotes its location.
+        If no match found, success is False and index denotes first available
+        slot.
+        """
+        firstAvail = None
+        for _ in range(len(self._table)):
+            if self._is_available(j):
+                if firstAvail is None:
+                    firstAvail = j
+                if self._table[j] is None:
+                    return (False, firstAvail)
+            elif k == self._table[j]._key:
+                return (True, j)
+            j = (j + 1) % (len(self._table))
+        return (False, firstAvail)
+
+    def _bucket_getitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError("Key Error: " + repr(k)) # No match found
+        return self._table[s]._value
+
+    def _bucket_setitem(self, j, k, v):
+        found, s = self._find_slot(j, k)
+        if not found:
+            self._table[s] = self._Item(k, v)
+            self._n += 1
+        else:
+            self._table[s]._value = v
+
+    def _bucket_delitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError("Key Error: " + repr(k)) # No match found
+        self._table[s] = ProbeHashMap._AVAIL # mark as vacated
+
+    def __iter__(self):
+        for j in range(len(self._table)):
+            if not self._is_available(j):
+                yield self._table[j]._key
