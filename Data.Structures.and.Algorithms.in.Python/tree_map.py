8edb5370a6b996108e1cab8ce3e8c43f425ba952
Jiaming Liu <storypku@gmail.com>
2013-08-09 00:16:43 +0800

    rotation utilities

--- Data.Structures.and.Algorithms.in.Python/tree_map.py
+++ Data.Structures.and.Algorithms.in.Python/tree_map.py
@@ -263,3 +263,45 @@ class TreeMap(LinkedBinaryTree, MapBase):
 
     def _rebalance_delete(self, pos):
         pass
+
+    def _relink(self, parent, child, make_left_child):
+        """Relink parent node with child node (child could be None)."""
+        if make_left_child:
+            parent._left = child
+        else:
+            parent._right = child
+        if child is not None: # make child point to parent
+            child._parent = parent
+
+    def _rotate(self, pos):
+        """Rotate Position pos above its parent."""
+        x = pos._node
+        y = x._parent
+        z = y._parent       # grandparent (possibly None)
+        if z is None:
+            self._root =x   # x becomes root
+            x._parent = None
+        else:
+            self._relink(z, x, y == z._left)    # x becomes direct child of z
+        # now rotate x and y, including transfer of middle subtree
+        if x == y._left:
+            self._relink(y, x._right, True) # x._right becomes left child of y
+            self._relink(x, y, False)       # y becomes right child of x
+        else:
+            self._relink(y, x._left, False) # x._left becomes right child of y
+            self._relink(x, y, True)        # y becomes left child of x
+
+    def _restructure(self, x):
+        """Perform trinode restructure of Position x with parent / grand-
+        parent."""
+        y = self.parent(x)
+        z = self.parent(y)
+        if (x == self.right(y)) == (y == self.right(z)): # matching alignments
+            self._rotate(y)                 # single rotation (of y)
+            return y                        # y is new subtree's root
+        else:       # opposite alignments performs double rotation (of x)
+            self._rotate(x)
+            self._rotate(x)
+            return x                        # x is new subtree's root
+
+
