b64e9c549f2b380ac692b22f9de2f65b145b0552
Jiaming Liu <storypku@gmail.com>
2013-08-31 23:22:12 +0800

    from timer import Timer; with Timer() as t: blabla

---
+++ Python3.Object.Oriented.Programming/timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer(object):
+	def __init__(self, verbose=False):
+		self.verbose = verbose
+
+	def __enter__(self):
+		self.start = time.time()
+		return self
+
+	def __exit__(self, *args):
+		self.end = time.time()
+		self.secs = self.end - self.start
+		if self.verbose:
+			print("elapsed time: %f ms" % (1000*self.secs))
